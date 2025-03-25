d59d83b69e2d4d80c6ddd7fe391c45f9a5ee410d
HoTienQuoc <hotienquoc0429@gmail.com>
2025-03-25 10:17:09 +0700

    Bai Tong Hop GK

--- TongHopGK/fragment/src/main/java/com/example/fragment/fragment/controller/ViewController.java
+++ TongHopGK/fragment/src/main/java/com/example/fragment/fragment/controller/ViewController.java
@@ -1,12 +1,48 @@
 package com.example.fragment.fragment.controller;
 
+import com.example.fragment.fragment.model.Student;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ViewController {
-    @GetMapping("")
+    private List<Student> students = new ArrayList<>();
+
+
+    @GetMapping("/")
     public String index() {
         return "index";
     }
+
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/list")
+    public String showStudentList(Model model) {
+        model.addAttribute("students", students);
+        return "list";
+    }
+
+    @GetMapping("/addNew")
+    public String showAddStudentForm(Model model) {
+        model.addAttribute("student", new Student());
+        return "addNew";
+    }
+
+    @PostMapping("/addNew")
+    public String addStudent(@ModelAttribute Student student) {
+        students.add(student);
+        return "redirect:/list";
+    }
+
+
 }
---
+++ TongHopGK/fragment/src/main/java/com/example/fragment/fragment/model/Student.java
@@ -0,0 +1,31 @@
+package com.example.fragment.fragment.model;
+
+public class Student {
+    private String mssv;
+    private String name;
+    private String diemTB;
+
+    public String getMssv() {
+        return mssv;
+    }
+
+    public void setMssv(String mssv) {
+        this.mssv = mssv;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDiemTB() {
+        return diemTB;
+    }
+
+    public void setDiemTB(String diemTB) {
+        this.diemTB = diemTB;
+    }
+}
